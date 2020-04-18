98608417ec323e7c75a1ebfaf43967b014050b11
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-04-18 11:20:12 +0530

    Feature added to reset and display the board

--- ticTac.sh
+++ ticTac.sh
@@ -1,1 +1,24 @@
 echo "welcome";
+
+declare board;
+# resets the board to inetial stage
+function resetBoard() {
+    for (( counter=1; counter<=9; counter++))
+    do
+        board[$counter]="-";
+    done
+}
+
+# display the board on console
+function displayBoard() {
+    for (( counter=1; counter<=9; counter++))
+    do
+        printf " ${board[$counter]}";
+        if (( $counter % 3 == 0)); then
+            printf "\n";
+        fi
+    done
+}
+
+resetBoard;
+displayBoard;
