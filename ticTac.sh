4ec9f6fc918d9270c7786fe9af31a1537464df5d
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-04-18 13:09:21 +0530

    Added feature to check game winning status

--- ticTac.sh
+++ ticTac.sh
@@ -8,8 +8,8 @@ function resetBoard() {
     for (( counter=1; counter<=9; counter++))
     do
         board[$counter]="-";
-    done    
-}
+    done  
+}  
 
 # display the board on console
 function displayBoard() {
@@ -26,13 +26,13 @@ function displayBoard() {
 function assigneeSymbolAndStart() {
     toss=$(( RANDOM % 2));
     if (( $toss == 0)); then
-        userSymbol=o;
-        syatemSymbol=x;
+        userSymbol=o";"
+        syatemSymbol="x";
         echo "it's my turn";
         echo "you are playing with 'o'";
     else
-        userSymbol=x;
-        syatemSymbol=o;
+        userSymbol="x";
+        syatemSymbol="o";
         echo "it's your turn";
         echo "you are playing with 'x'";
         displayBoard;
@@ -49,6 +49,80 @@ function userTurn() {
     fi
     board[$userMove]=$userSymbol;
     displayBoard;
+    checkGameStatus $userSymbol;
+}
+
+
+
+function checkRowPattern() {
+    checkingSymbole=$1;
+    position=$2
+    if [[ $checkingSymbole == ${board[$(($position + 1))]} && $checkingSymbole == ${board[$(($position + 2))]} ]]; then
+        return 1;
+    else
+        return 0;
+    fi
+}
+function checkColumnPattern() {
+    checkingSymbole=$1;
+    position=$2
+    if [[ $checkingSymbole == ${board[$(($position + 3))]} && $checkingSymbole == ${board[$(($position + 6))]} ]]; then
+        return 1;
+    else
+        return 0;
+    fi
+}
+function checkCrossPattern() {
+    checkingSymbole=$1;
+    position=$2;
+    increment=4;
+    if (( $position == 3)); then
+        increment=2;
+    fi
+    if [[ $checkingSymbole == ${board[$(($position + $increment))]} && $checkingSymbole == ${board[$(($position + $((2 * $increment)) ))]} ]]; then
+        return 1;
+    else
+        return 0;
+    fi
+}
+
+function checkWiningPattern() {
+    checkingSymbole=$1;
+    position=$2;
+    checkRowPattern $checkingSymbole $position;
+    if (( $? == 0 )); then    
+        checkColumnPattern $checkingSymbole $position;
+        if (( $? == 0 )); then   
+            checkCrossPattern $checkingSymbole $position;
+            if (( $? == 0 )); then  
+                return 0;
+            else
+                return 1;
+            fi 
+        else
+            return 1;
+        fi 
+    else
+        return 1;
+    fi
+}
+
+function checkGameStatus() {
+    symbole=$1;
+    for (( i=1; i<=7; i++))
+    do
+        if [[ ${board[$i]} == $symbole ]]; then
+            checkWiningPattern $symbole $i;
+            if (( $? == 1)); then
+                echo "game over";
+                displayBoard;
+                break;
+            fi
+        fi
+        if (( $i == 4)); then
+            i=$(($i+2));
+        fi   
+    done
 }
 
 resetBoard;
